1e48ccef4bb9c99c7aa4820358d9250be2aa4fc2
Lukas Pohlreich <pohlreichlukas@gmail.com>
2015-03-07 23:27:08 +0100

    Example of template rendering

--- controllers/HomepageController.php
+++ controllers/HomepageController.php
@@ -2,10 +2,12 @@
 namespace DemoApp\Controllers;
 
 use MicroFW\Http\Response;
+use MicroFW\Templating\Template;
 
 function homepage($request, $args)
 {
-    return new Response('Homepage');
+    $template = new Template('homepage.html');
+    return new Response($template->render());
 }
 
 function optionsList($request, $args)
